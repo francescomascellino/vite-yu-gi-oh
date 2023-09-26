97fc82f76903402832bccd5bf728eb4e89c0d3c0
francescomascellino <franesco.mascellino@gmail.com>
2023-09-26 18:46:05 +0200

    fix typo that prevented component to load

--- src/components/AppCardsContainer.vue
+++ src/components/AppCardsContainer.vue
@@ -2,118 +2,14 @@
     <!-- CARDS CONTAINER -->
     <div class="row row-cols-2 row-cols-md-3 row-cols-lg-6 g-4" v-if="store.cards">
 
-
         <AppCards />
 
     </div>
 
     <!-- LOADER -->
-
     <div class="row row-cols-2 row-cols-md-3 row-cols-lg-6 g-4 justify-content-center" v-else>
 
-        <!-- FAKE CARDS -->
-
-        <div class="col">
-
-            <div class="card shadow ygo-card ygo-cardLoader">
-
-                <div class="ygo-imgLoader shine"></div>
-
-                <div class="card-body p-2">
-
-                    <h6 class="card-title my-3 ygo-cardTitle shine"></h6>
-                    <p class="card-text my-3 ygo-cardText shine"></p>
-
-                </div>
-
-            </div>
-
-        </div>
-
-        <div class="col">
-
-            <div class="card shadow ygo-card ygo-cardLoader">
-
-                <div class="ygo-imgLoader shine"></div>
-
-                <div class="card-body p-2">
-
-                    <h6 class="card-title my-3 ygo-cardTitle shine"></h6>
-                    <p class="card-text my-3 ygo-cardText shine"></p>
-
-                </div>
-
-            </div>
-
-        </div>
-
-        <div class="col">
-
-            <div class="card shadow ygo-card ygo-cardLoader">
-
-                <div class="ygo-imgLoader shine"></div>
-
-                <div class="card-body p-2">
-
-                    <h6 class="card-title my-3 ygo-cardTitle shine"></h6>
-                    <p class="card-text my-3 ygo-cardText shine"></p>
-
-                </div>
-
-            </div>
-
-        </div>
-
-        <div class="col">
-
-            <div class="card shadow ygo-card ygo-cardLoader">
-
-                <div class="ygo-imgLoader shine"></div>
-
-                <div class="card-body p-2">
-
-                    <h6 class="card-title my-3 ygo-cardTitle shine"></h6>
-                    <p class="card-text my-3 ygo-cardText shine"></p>
-
-                </div>
-
-            </div>
-
-        </div>
-
-        <div class="col">
-
-            <div class="card shadow ygo-card ygo-cardLoader">
-
-                <div class="ygo-imgLoader shine"></div>
-
-                <div class="card-body p-2">
-
-                    <h6 class="card-title my-3 ygo-cardTitle shine"></h6>
-                    <p class="card-text my-3 ygo-cardText shine"></p>
-
-                </div>
-
-            </div>
-
-        </div>
-
-        <div class="col">
-
-            <div class="card shadow ygo-card ygo-cardLoader">
-
-                <div class="ygo-imgLoader shine"></div>
-
-                <div class="card-body p-2">
-
-                    <h6 class="card-title my-3 ygo-cardTitle shine"></h6>
-                    <p class="card-text my-3 ygo-cardText shine"></p>
-
-                </div>
-
-            </div>
-
-        </div>
+        <AppCardsLoader />
 
     </div>
 </template>
@@ -122,13 +18,15 @@
 
 // IMPORTA STATE GLOBALE DA store.js PER NON DOVER PASSARE LE PROP DA COMPONENTE A COMPONENTE E CREARE UN PROP DRILLING
 import { store } from '../store';
-import AppCards from './AppCards.Vue'
+import AppCards from './AppCards.Vue';
+import AppCardsLoader from './AppCardsLoader.vue'
 
 export default {
     name: "AppCardsContainer",
 
     components: {
-        AppCards
+        AppCards,
+        AppCardsLoader
     },
 
     data() {
@@ -151,34 +49,6 @@ export default {
 <style lang="scss" scoped>
 @use '../assets/scss/partials/variables.scss' as *;
 
-/*#region SHINE */
 
-.ygo-cardLoader {
-    min-height: 290px;
-
-    .ygo-imgLoader {
-        height: 180px;
-    }
-
-}
-
-.shine {
-    background: linear-gradient(110deg, #dab47f 8%, $ygo-orange 18%, #dab47f 33%);
-    background-size: 200% 100%;
-    animation: 1s shine linear infinite;
-}
-
-h6.shine,
-p.shine {
-    padding: 8px 16px;
-    line-height: 2rem;
-}
-
-@keyframes shine {
-    to {
-        background-position-x: -200%;
-    }
-}
-
-/*#endregion SHINE */
+@use '../assets/scss/partials/AppCards.scss';
 </style>
---
+++ src/components/AppCardsLoader.vue
@@ -0,0 +1,116 @@
+<template>
+    <!-- FAKE CARDS -->
+    <div class="col">
+
+        <div class="card shadow ygo-card ygo-cardLoader">
+
+            <div class="ygo-imgLoader shine"></div>
+
+            <div class="card-body p-2">
+
+                <h6 class="card-title my-3 ygo-cardTitle shine"></h6>
+                <p class="card-text my-3 ygo-cardText shine"></p>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="col">
+
+        <div class="card shadow ygo-card ygo-cardLoader">
+
+            <div class="ygo-imgLoader shine"></div>
+
+            <div class="card-body p-2">
+
+                <h6 class="card-title my-3 ygo-cardTitle shine"></h6>
+                <p class="card-text my-3 ygo-cardText shine"></p>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="col">
+
+        <div class="card shadow ygo-card ygo-cardLoader">
+
+            <div class="ygo-imgLoader shine"></div>
+
+            <div class="card-body p-2">
+
+                <h6 class="card-title my-3 ygo-cardTitle shine"></h6>
+                <p class="card-text my-3 ygo-cardText shine"></p>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="col">
+
+        <div class="card shadow ygo-card ygo-cardLoader">
+
+            <div class="ygo-imgLoader shine"></div>
+
+            <div class="card-body p-2">
+
+                <h6 class="card-title my-3 ygo-cardTitle shine"></h6>
+                <p class="card-text my-3 ygo-cardText shine"></p>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="col">
+
+        <div class="card shadow ygo-card ygo-cardLoader">
+
+            <div class="ygo-imgLoader shine"></div>
+
+            <div class="card-body p-2">
+
+                <h6 class="card-title my-3 ygo-cardTitle shine"></h6>
+                <p class="card-text my-3 ygo-cardText shine"></p>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="col">
+
+        <div class="card shadow ygo-card ygo-cardLoader">
+
+            <div class="ygo-imgLoader shine"></div>
+
+            <div class="card-body p-2">
+
+                <h6 class="card-title my-3 ygo-cardTitle shine"></h6>
+                <p class="card-text my-3 ygo-cardText shine"></p>
+
+            </div>
+
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+
+    name: "AppCardsLoader"
+
+}
+</script>
+
+<style lang="scss" scoped>
+@use '../assets/scss/partials/AppCardsLoader.scss';
+</style>
