3436e85f6619b7ec50b08f898dd16682a21b7be0
francescomascellino <franesco.mascellino@gmail.com>
2023-09-26 15:36:19 +0200

    add card data from api on markup

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,24 @@
 <template>
-    <div>
-        <h1 class=" text-danger">TITLE</h1>
-    </div>
+    <header>
+        <h1 class="text-center"> Yu-Gi_OH! Vite </h1>
+    </header>
+    <main>
+        <div class="container">
+
+            <div class="row row-cols-2 row-cols-md-3 row-cols-lg-6">
+                <div class="col" v-for="card in store.cards">
+                    <div class="card">
+                        <img class="card-img-top" :src="card.card_images[0].image_url" alt="Title">
+                        <div class="card-body">
+                            <h4 class="card-title">{{ card.name }}</h4>
+                            <p class="card-text">{{ card.type }}</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </main>
 </template>
 
 <script>
@@ -21,6 +38,7 @@ export default {
 
     mounted() {
 
+        //USA LA FUNZIONE PER RICHIAMARE I DATI DALLA API CONTENTUTA DENTRO "store"
         store.fetchData()
 
     }
